bc96c9228cb56ea102d3abaa221937b67d66dbee
BTMLTECH <technology@btmlimited.net>
2026-03-17 14:20:58 +0100

    updated holidays32026

--- src/utils/holidays.ts
+++ src/utils/holidays.ts
@@ -64,26 +64,27 @@
 // };
 
 // Nigerian Public Holidays
-export const nigerianPublicHolidays2025 = [
-  { name: "New Year's Day", date: "2025-01-01" },
-  { name: "Eid al-Fitr", date: "2025-03-31" },
-  { name: "Eid al-Fitr Holiday", date: "2025-04-01" },
-  { name: "Good Friday", date: "2025-04-18" },
-  { name: "Easter Monday", date: "2025-04-21" },
-  { name: "Workers' Day", date: "2025-05-01" },
-  { name: "Children's Day", date: "2025-05-27" },
-  { name: "Eid al-Adha", date: "2025-06-07" },
-  { name: "Eid al-Adha Holiday", date: "2025-06-08" },
-  { name: "Democracy Day", date: "2025-06-12" },
-  { name: "Eid al-Maulud", date: "2025-09-05" },
-  { name: "Independence Day", date: "2025-10-01" },
-  { name: "Christmas Day", date: "2025-12-25" },
-  { name: "Boxing Day", date: "2025-12-26" },
+export const nigerianPublicHolidays2026 = [
+  { name: "New Year's Day", date: "2026-01-01" },
+  { name: "Eid al-Fitr", date: "2026-03-20" },
+  { name: "Eid al-Fitr Holiday", date: "2026-03-23" },
+  { name: "Good Friday", date: "2026-04-03" },
+  { name: "Easter Monday", date: "2026-04-06" },
+  { name: "Workers' Day", date: "2026-05-01" },
+  { name: "Children's Day", date: "2026-05-27" }, 
+  { name: "Eid al-Adha", date: "2026-05-27" },
+  { name: "Eid al-Adha Holiday", date: "2026-05-28" },
+  { name: "Democracy Day", date: "2026-06-12" },
+  { name: "Eid al-Maulud", date: "2026-08-26" }, 
+  { name: "Independence Day", date: "2026-10-01" },
+  { name: "Christmas Day", date: "2026-12-25" },
+  { name: "Boxing Day", date: "2026-12-28" },
 ];
+
 
 export const isPublicHoliday = (date: Date): boolean => {
   const dateString = date.toISOString().split("T")[0];
-  return nigerianPublicHolidays2025.some(
+  return nigerianPublicHolidays2026.some(
     (holiday) => holiday.date === dateString
   );
 };
@@ -133,7 +134,7 @@ export const getHolidaysInRange = (
   ) {
     if (isPublicHoliday(date)) {
       const dateStr = date.toISOString().split("T")[0];
-      const holiday = nigerianPublicHolidays2025.find(
+      const holiday = nigerianPublicHolidays2026.find(
         (h) => h.date === dateStr
       );
       holidays.push({
